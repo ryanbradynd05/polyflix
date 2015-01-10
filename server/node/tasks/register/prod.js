f4e12f60a8779a892c6fa147b8ea09dd081ba7e9
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-09 22:39:02 -0500

    Create Sails app
    Sails Gulp app with a Movies model created

---
+++ server/node/tasks/register/prod.js
@@ -0,0 +1,14 @@
+module.exports = function (gulp, plugins) {
+	gulp.task('prod', function(cb) {
+		plugins.sequence(
+			'compileAssets',
+			'concat:js',
+			'concat:css',
+			'uglify:dist',
+			'cssmin:dist',
+			'sails-linker-gulp:prodAssets',
+			'sails-linker-gulp:prodViews',
+			cb
+		);
+	});
+};
