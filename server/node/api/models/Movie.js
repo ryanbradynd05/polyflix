2d8e46c71edc622b5458c60ed8610abc6e484239
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-11 04:55:00 -0500

    Node Update movies table name
    Set table name to plural and integer column to default size

--- server/node/api/models/Movie.js
+++ server/node/api/models/Movie.js
@@ -1,11 +1,12 @@
 /**
-* Movie.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Movie.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
+  tableName: 'movies',
 
   attributes: {
     title: {
@@ -13,8 +14,7 @@ module.exports = {
     },
     themoviedbid: {
       type: 'integer',
-      size: 7
+      size: 11
     }
   }
 };
-
