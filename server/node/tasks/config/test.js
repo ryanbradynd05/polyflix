7cca4bb602faa92d41a627d825625933341d0128
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-10 22:33:21 -0500

    Increase mocha timeout

--- server/node/tasks/config/test.js
+++ server/node/tasks/config/test.js
@@ -13,7 +13,7 @@ module.exports = function(gulp, plugins, growl) {
       .pipe(plugins.istanbul.hookRequire())
       .on('finish', function() {
         gulp.src(['test/**/*.js'])
-          .pipe(plugins.mocha())
+          .pipe(plugins.mocha({timeout: 5000}))
           .pipe(plugins.istanbul.writeReports()) // Creating the reports after tests runned
           .pipe(plugins.if(growl, plugins.notify({
             message: 'test task complete'
