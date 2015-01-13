d6610a85fc2b3d8bb49d4c999a77a9cf2c55d21e
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-12 20:38:57 -0500

    Rails add movie serializer

--- server/rails/app/controllers/movies_controller.rb
+++ server/rails/app/controllers/movies_controller.rb
@@ -1,2 +1,34 @@
 class MoviesController < ApplicationController
+    def index
+        render json: Movie.all, each_serializer: MovieSerializer, root: 'movies'
+    end
+
+    def show
+        render json: Movie.find(params[:id]), root: 'movie'
+    end
+
+    def new
+        render json: Movie.new, root: 'movie'
+    end
+
+    def create
+        @movie = Movie.new(params[:movie])
+
+        @movie.save
+        render json: @movie, root: 'movie'
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+
+        @movie.update(params[:movie])
+        render json: @movie, root: 'movie'
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+
+        render json: @movie, root: 'movie'
+    end
 end
