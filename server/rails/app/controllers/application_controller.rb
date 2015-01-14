094d20d54a0a0733ca7b75a171a976c55cea857e
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-13 21:05:58 -0500

    Add themoviedb config

--- server/rails/app/controllers/application_controller.rb
+++ server/rails/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    require 'themoviedb'
+    require 'yaml'
+
+
+    before_filter :set_config
+    config = YAML.load_file(File.join("#{Rails.root}/config", "local.yml"))
+    themoviedbkey = config['themoviedb']['key']
+    Tmdb::Api.key("#{themoviedbkey}")
+
+    def set_config
+        @configuration = Tmdb::Configuration.new
+    end
 end
