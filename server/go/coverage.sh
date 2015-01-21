99b9a13a0ac0a38ee93b508b0a026c2539406ac9
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-20 23:52:00 -0500

    Go coverage work in progress

---
+++ server/go/coverage.sh
@@ -0,0 +1,18 @@
+echo "Testing Code Coverage"
+BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+cd "${BASEDIR}/src/polyflix"
+gocov test > ${BASEDIR}/coverage/coverage.json
+gocov-html ${BASEDIR}/coverage/coverage.json > ${BASEDIR}/coverage/coverage_report.html
+
+cd "${BASEDIR}/src/polyflix/controllers"
+gocov test > ${BASEDIR}/coverage/coverage.json
+gocov-html ${BASEDIR}/coverage/coverage.json > ${BASEDIR}/coverage/coverage_report.html
+
+cd "${BASEDIR}/src/polyflix/database"
+gocov test > ${BASEDIR}/coverage/coverage.json
+gocov-html ${BASEDIR}/coverage/coverage.json > ${BASEDIR}/coverage/coverage_report.html
+
+cd "${BASEDIR}/src/polyflix/models"
+gocov test > ${BASEDIR}/coverage/coverage.json
+gocov-html ${BASEDIR}/coverage/coverage.json > ${BASEDIR}/coverage/coverage_report.html
