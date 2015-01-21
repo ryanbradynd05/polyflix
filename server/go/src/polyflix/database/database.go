9f952f6eb820cb9f4f13018e474d9bf0a2e207a0
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-20 23:57:00 -0500

    Go Read DB config from YAML

--- server/go/src/polyflix/database/database.go
+++ server/go/src/polyflix/database/database.go
@@ -15,15 +15,16 @@ type Database struct {
 var _init_ctx sync.Once
 var _instance *Database
 
-func New() gorm.DB {
+func New(dbConn string) gorm.DB {
 	_init_ctx.Do(func() {
 		_instance = new(Database)
-		db, err := gorm.Open("mysql", "root:rootpw@/polyflix-go-dev?charset=utf8&parseTime=True")
+		connString := fmt.Sprintf("%s", dbConn)
+		db, err := gorm.Open("mysql", connString)
 		if err != nil {
 			fmt.Printf("Gorm error: %+v\n", err)
 			os.Exit(1)
 		}
-		fmt.Printf("Database opened")
+		fmt.Printf("Database opened\n")
 		db.DB()
 		db.DB().Ping()
 		db.DB().SetMaxIdleConns(10)
--- server/go/src/polyflix/server.go
+++ server/go/src/polyflix/server.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/kylelemons/go-gypsy/yaml"
 	"net/http"
 	"polyflix/controllers"
 	"polyflix/database"
 )
 
 var (
-	url = "localhost:3000"
+	url    = "localhost:3000"
+	env    string
+	dbConn string
 )
 
 func main() {
+	LoadConfig()
 	http.Handle("/", Handlers())
 	http.ListenAndServe(url, nil)
 }
 
+func LoadConfig() {
+	config, _ := yaml.ReadFile("db/dbconf.yml")
+	flag.StringVar(&env, "env", "development", "Environment")
+	flag.Parse()
+	fmt.Printf("Load %v config\n", env)
+	conn, _ := config.Get(fmt.Sprintf("%s.open", env))
+	dbConn = conn
+	fmt.Printf("DB Connection:  %v\n", dbConn)
+}
+
+func DbInit() gorm.DB {
+	db := database.New(dbConn)
+	return db
+}
+
 func Handlers() *mux.Router {
-	db := database.New()
+	db := DbInit()
 	router := mux.NewRouter().StrictSlash(true)
 	movies := router.PathPrefix("/movies").Subrouter().StrictSlash(true)
 	moviesController := controllers.MoviesController{DB: db}
