2576de216512a318f3f1c4e30121467cb4f9d298
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-22 23:54:05 -0500

    Go Refractor models package into controller

--- server/go/src/polyflix/controllers/moviesController.go
+++ server/go/src/polyflix/controllers/moviesController.go
@@ -5,9 +5,24 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	"net/http"
-	. "polyflix/models"
 	"strconv"
 )
+
+// ArrayPayload struct to hold multiple movies for JSON marshalling
+type ArrayPayload struct {
+	Movies []Movie `json:"movies"`
+}
+
+// SinglePayload struct to hold a single movie for JSON marshalling
+type SinglePayload struct {
+	Movie Movie `json:"movie"`
+}
+
+// Movie type for movie objects
+type Movie struct {
+	Title        string `json:"title"`
+	Themoviedbid int    `json:"themoviedbid"`
+}
 
 // MoviesController controller for the movies subroute
 type MoviesController struct {
