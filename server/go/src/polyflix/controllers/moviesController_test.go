0c9d66fe3cf49fb718cd7a8f2ba26064853f0cbf
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-23 00:34:51 -0500

    Go move model tests to controller

--- server/go/src/polyflix/controllers/moviesController_test.go
+++ server/go/src/polyflix/controllers/moviesController_test.go
@@ -1,1 +1,40 @@
-package controllers
+package controllers_test
+
+import (
+	. "gopkg.in/check.v1"
+	. "polyflix/controllers"
+	"testing"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type MovieControllerSuite struct{}
+
+var _ = Suite(&MovieControllerSuite{})
+
+func (s *MovieControllerSuite) TestMovie(c *C) {
+	movie := Movie{Title: "Fight Club", Themoviedbid: 500}
+	c.Assert(movie.Title, FitsTypeOf, string("Fight Club"))
+	c.Assert(movie.Themoviedbid, FitsTypeOf, int(500))
+	c.Assert(movie, FitsTypeOf, Movie{})
+}
+
+func (s *MovieControllerSuite) TestSinglePayload(c *C) {
+	movie := Movie{Title: "Fight Club", Themoviedbid: 500}
+	payload := SinglePayload{Movie: movie}
+	c.Assert(payload.Movie.Title, FitsTypeOf, string("Fight Club"))
+	c.Assert(payload.Movie.Themoviedbid, FitsTypeOf, int(500))
+	c.Assert(payload.Movie, FitsTypeOf, Movie{})
+	c.Assert(payload, FitsTypeOf, SinglePayload{})
+}
+
+func (s *MovieControllerSuite) TestArrayPayload(c *C) {
+	movie := Movie{Title: "Fight Club", Themoviedbid: 500}
+	movies := []Movie{movie}
+	payload := ArrayPayload{Movies: movies}
+	c.Assert(payload.Movies[0].Title, FitsTypeOf, string("Fight Club"))
+	c.Assert(payload.Movies[0].Themoviedbid, FitsTypeOf, int(500))
+	c.Assert(payload.Movies[0], FitsTypeOf, Movie{})
+	c.Assert(payload, FitsTypeOf, ArrayPayload{})
+}
--- server/go/src/polyflix/database/database_test.go
+++ server/go/src/polyflix/database/database_test.go
@@ -1,1 +1,1 @@
-package database
+package database_test
