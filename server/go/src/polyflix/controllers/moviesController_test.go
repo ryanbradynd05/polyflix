45b57145b9dd999b68a3535b9c6b47eaa39b7d00
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-23 23:50:00 -0500

    Go Add MoviesController.DestroyHandler test

--- server/go/src/polyflix/controllers/moviesController_test.go
+++ server/go/src/polyflix/controllers/moviesController_test.go
@@ -82,3 +82,15 @@ func (s *MovieControllerSuite) TestShowHandler(c *C) {
 
 	c.Assert(recorder.Code, Equals, 200)
 }
+
+func (s *MovieControllerSuite) TestDestroyHandler(c *C) {
+	controller := MoviesController{DB: s.db}
+	recorder := httptest.NewRecorder()
+	url := fmt.Sprintf("%s/movies/1", s.server.URL)
+	req, err := http.NewRequest("DELETE", url, nil)
+	c.Assert(err, IsNil)
+
+	controller.ShowHandler(recorder, req)
+
+	c.Assert(recorder.Code, Equals, 200)
+}
