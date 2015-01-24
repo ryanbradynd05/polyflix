a3ee378fb6c9997cd13e12d5667f634da8381548
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-23 23:35:00 -0500

    Go Add MoviesController.Index test

--- server/go/src/polyflix/controllers/moviesController_test.go
+++ server/go/src/polyflix/controllers/moviesController_test.go
@@ -1,17 +1,37 @@
 package controllers_test
 
 import (
+	"fmt"
+	"github.com/jinzhu/gorm"
 	. "gopkg.in/check.v1"
+	"net/http"
+	"net/http/httptest"
+	"polyflix"
 	. "polyflix/controllers"
 	"testing"
+)
+
+var (
+	url    = "localhost:3000"
+	dbConn string
 )
 
 // Hook up gocheck into the "go test" runner.
 func Test(t *testing.T) { TestingT(t) }
 
-type MovieControllerSuite struct{}
+type MovieControllerSuite struct {
+	server *httptest.Server
+	db     gorm.DB
+}
 
 var _ = Suite(&MovieControllerSuite{})
+
+func (s *MovieControllerSuite) SetUpSuite(c *C) {
+	env := "test"
+	dbConn := main.GetDbConn(env)
+	s.db = main.DbInit(dbConn)
+	s.server = httptest.NewServer(main.Handlers(s.db))
+}
 
 func (s *MovieControllerSuite) TestMovie(c *C) {
 	movie := Movie{Title: "Fight Club", Themoviedbid: 500}
@@ -37,4 +57,16 @@ func (s *MovieControllerSuite) TestArrayPayload(c *C) {
 	c.Assert(payload.Movies[0].Themoviedbid, FitsTypeOf, int(500))
 	c.Assert(payload.Movies[0], FitsTypeOf, Movie{})
 	c.Assert(payload, FitsTypeOf, ArrayPayload{})
+}
+
+func (s *MovieControllerSuite) TestIndexHandler(c *C) {
+	controller := MoviesController{DB: s.db}
+	recorder := httptest.NewRecorder()
+	url := fmt.Sprintf("%s/movies", s.server.URL)
+	req, err := http.NewRequest("GET", url, nil)
+	c.Assert(err, IsNil)
+
+	controller.IndexHandler(recorder, req)
+
+	c.Assert(recorder.Code, Equals, 200)
 }
