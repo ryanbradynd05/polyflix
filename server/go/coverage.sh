8cc3d3a08ce4abed2b903099f4f302f0ca8b3b42
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-25 23:24:52 -0500

    Cleanup go coverage report

--- server/go/coverage.sh
+++ server/go/coverage.sh
@@ -2,13 +2,13 @@ echo "Testing Code Coverage"
 BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd "${BASEDIR}/src/polyflix"
-gocov test > ${BASEDIR}/coverage/coverage.json
-gocov-html ${BASEDIR}/coverage/coverage.json > ${BASEDIR}/coverage/coverage_report.html
+gocov test > ${BASEDIR}/coverage/polyflix.json
+gocov-html -s ${BASEDIR}/coverage/coverage.css ${BASEDIR}/coverage/polyflix.json > ${BASEDIR}/coverage/coverage_report.html
 
 cd "${BASEDIR}/src/polyflix/controllers"
-gocov test > ${BASEDIR}/coverage/coverage.json
-gocov-html ${BASEDIR}/coverage/coverage.json >> ${BASEDIR}/coverage/coverage_report.html
+gocov test > ${BASEDIR}/coverage/controllers.json
+gocov-html -s ${BASEDIR}/coverage/coverage.css ${BASEDIR}/coverage/controllers.json >> ${BASEDIR}/coverage/coverage_report.html
 
 cd "${BASEDIR}/src/polyflix/database"
-gocov test > ${BASEDIR}/coverage/coverage.json
-gocov-html ${BASEDIR}/coverage/coverage.json >> ${BASEDIR}/coverage/coverage_report.html
+gocov test > ${BASEDIR}/coverage/database.json
+gocov-html -s ${BASEDIR}/coverage/coverage.css ${BASEDIR}/coverage/database.json >> ${BASEDIR}/coverage/coverage_report.html
