070c7e7debf2eec30b3b529b311b7987741461ef
Ryan Brady <ryanbradynd05@gmail.com>
2015-02-20 23:38:00 -0500

    Base test for movies controller

--- client/angular/src/app/controllers/movies.controller.spec.js
+++ client/angular/src/app/controllers/movies.controller.spec.js
@@ -1,11 +1,24 @@
 'use strict';
 
 describe('controllers', function() {
+  var httpBackend, scope, createController;
   beforeEach(module('polyflix'));
 
-  // it('should ....', inject(function($controller) {
-  //   console.log('controller',$controller);
-  //   var moviesCtrl = $controller('MoviesCtrl');
-  //   expect(moviesCtrl).toBeDefined();
-  // }));
+  beforeEach(inject(function($rootScope, $httpBackend, $controller) {
+    httpBackend = $httpBackend;
+    scope = $rootScope.$new();
+
+    createController = function() {
+        return $controller('MoviesCtrl', {
+            '$scope': scope
+        });
+    };
+  }));
+
+  describe('controller', function() {
+    it('should exist', function() {
+      var controller = createController();
+      expect(controller).toBeDefined();
+    });
+  });
 });
