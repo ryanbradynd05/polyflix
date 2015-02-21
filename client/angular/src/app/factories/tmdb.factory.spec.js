5506107ca5fe937a3384e373f95d66d83e162f8d
Ryan Brady <ryanbradynd05@gmail.com>
2015-02-21 01:39:18 -0500

    Move test mocks to global mocks file

--- client/angular/src/app/factories/tmdb.factory.spec.js
+++ client/angular/src/app/factories/tmdb.factory.spec.js
@@ -2,134 +2,25 @@
 
 describe('tmdb Factory', function() {
   var TmdbFactory, httpBackend;
-  var baseUrl = 'http://localhost:3000/';
-  var configUrl = baseUrl + 'configs';
-  var configResults = {
-    configs: [{
-      images: {
-        base_url: 'http://image.tmdb.org/t/p/', // jshint ignore:line
-        secure_base_url: 'https://image.tmdb.org/t/p/', // jshint ignore:line
-        backdrop_sizes: ['w300', 'w780', 'w1280', 'original'], // jshint ignore:line
-        logo_sizes: ['w45', 'w92', 'w154', 'w185', 'w300', 'w500', 'original'], // jshint ignore:line
-        poster_sizes: ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original'], // jshint ignore:line
-        profile_sizes: ['w45', 'w185', 'h632', 'original'], // jshint ignore:line
-        still_sizes: ['w92', 'w185', 'w300', 'original'] // jshint ignore:line
-      }
-    }]
-  };
-  var allMoviesUrl = baseUrl + 'movies';
-  var allMoviesResults = {
-    movies: [{
-      title: 'Fight Club',
-      themoviedbid: 550,
-      id: 1,
-      createdAt: '2015-02-13T05:44:34.000Z',
-      updatedAt: '2015-02-13T05:44:34.000Z'
-    }, {
-      title: 'Guardians of the Galaxy',
-      themoviedbid: 118340,
-      id: 2,
-      createdAt: '2015-02-13T06:19:29.000Z',
-      updatedAt: '2015-02-13T06:19:29.000Z'
-    }]
-  };
-  var createMovieUrl = baseUrl + 'movies';
-  var createMovieResults = {
-    movie: {
-      title: 'The Matrix',
-      themoviedbid: 603,
-      id: 3,
-      createdAt: '2015-02-13T05:44:34.000Z',
-      updatedAt: '2015-02-13T05:44:34.000Z'
-    }
-  };
-  var searchUrl = baseUrl + 'movies/search/SDF';
-  var searchResults = {
-    page: 1,
-    results: [{
-      adult: false,
-      backdrop_path: '/qLxJlnfZ9b2BMzvhzvnJMBi7EGH.jpg', // jshint ignore:line
-      id: 14114,
-      original_title: 'You Got Served', // jshint ignore:line
-      release_date: '2004-01-30', // jshint ignore:line
-      poster_path: '/3ESy6TiBPSYIbEES1zNspdYtMtw.jpg', // jshint ignore:line
-      popularity: 0.33499858390984,
-      title: 'You Got Served',
-      video: false,
-      vote_average: 6.3, // jshint ignore:line
-      vote_count: 9 // jshint ignore:line
-    }],
-    total_pages: 1, // jshint ignore:line
-    total_results: 1 // jshint ignore:line
-  };
-  var infoUrl = baseUrl + 'movies/info/550';
-  var infoResults = {
-    adult: false,
-    backdrop_path: '/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg', // jshint ignore:line
-    belongs_to_collection: null, // jshint ignore:line
-    budget: 63000000,
-    genres: [{
-      id: 18,
-      name: 'Drama'
-    }],
-    homepage: '',
-    id: 550,
-    imdb_id: 'tt0137523', // jshint ignore:line
-    original_language: 'en', // jshint ignore:line
-    original_title: 'Fight Club', // jshint ignore:line
-    overview: 'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground " fight clubs " forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
-    popularity: 3.16634464772838,
-    poster_path: '/2lECpi35Hnbpa4y46JX0aY3AWTy.jpg', // jshint ignore:line
-    production_companies: [{ // jshint ignore:line
-      name: '20th Century Fox',
-      id: 25
-    }, {
-      name: 'Fox 2000 Pictures',
-      id: 711
-    }, {
-      name: 'Regency Enterprises',
-      id: 508
-    }],
-    production_countries: [{ // jshint ignore:line
-      iso_3166_1: 'DE', // jshint ignore:line
-      name: 'Germany'
-    }, {
-      iso_3166_1: 'US', // jshint ignore:line
-      name: 'United States of America'
-    }],
-    release_date: '1999-10-14', // jshint ignore:line
-    revenue: 100853753,
-    runtime: 139,
-    spoken_languages: [{ // jshint ignore:line
-      iso_639_1: 'en', // jshint ignore:line
-      name: 'English'
-    }],
-    status: 'Released',
-    tagline: 'How much can you know about yourself if you\'ve never been in a fight?',
-    title: 'Fight Club',
-    video: false,
-    vote_average: 7.8, // jshint ignore:line
-    vote_count: 3286 // jshint ignore:line
-  };
 
   beforeEach(module('polyflix'));
 
   beforeEach(inject(function($injector) {
     httpBackend = $injector.get('$httpBackend');
-    httpBackend.whenGET(configUrl).respond(
-      JSON.stringify(configResults)
+    httpBackend.whenGET(mocks.configUrl).respond(
+      JSON.stringify(mocks.configResults)
     );
-    httpBackend.whenGET(allMoviesUrl).respond(
-      JSON.stringify(allMoviesResults)
+    httpBackend.whenGET(mocks.allMoviesUrl).respond(
+      JSON.stringify(mocks.allMoviesResults)
     );
-    httpBackend.whenPOST(createMovieUrl).respond(
-      JSON.stringify(createMovieResults)
+    httpBackend.whenPOST(mocks.createMovieUrl).respond(
+      JSON.stringify(mocks.createMovieResults)
     );
-    httpBackend.whenGET(searchUrl).respond(
-      JSON.stringify(searchResults)
+    httpBackend.whenGET(mocks.searchUrl).respond(
+      JSON.stringify(mocks.searchResults)
     );
-    httpBackend.whenGET(infoUrl).respond(
-      JSON.stringify(infoResults)
+    httpBackend.whenGET(mocks.infoUrl).respond(
+      JSON.stringify(mocks.infoResults)
     );
   }));
 
@@ -148,7 +39,7 @@ describe('tmdb Factory', function() {
       TmdbFactory.config();
       httpBackend.flush();
       var actualConfigImages = TmdbFactory.configuration.$object[0].images;
-      var expectedConfigImages = configResults.configs[0].images;
+      var expectedConfigImages = mocks.configResults.configs[0].images;
       expect(actualConfigImages).toEqual(expectedConfigImages);
     });
   });
@@ -158,7 +49,7 @@ describe('tmdb Factory', function() {
       var movies = TmdbFactory.all('movies');
       httpBackend.flush();
       var actualMovies = movies.$object;
-      var expectedMovies = allMoviesResults.movies;
+      var expectedMovies = mocks.allMoviesResults.movies;
       var config = TmdbFactory.configuration.$object[0];
       expect(config).toBeDefined();
       expect(actualMovies[0].title).toEqual(expectedMovies[0].title);
@@ -185,7 +76,7 @@ describe('tmdb Factory', function() {
       var movie = TmdbFactory.create('movies', newMovie);
       httpBackend.flush();
       var actualMovies = movie.$object.movie;
-      var expectedMovies = createMovieResults.movie;
+      var expectedMovies = mocks.createMovieResults.movie;
       var config = TmdbFactory.configuration.$object[0];
       expect(config).toBeDefined();
       expect(actualMovies.title).toEqual(expectedMovies.title);
@@ -201,7 +92,7 @@ describe('tmdb Factory', function() {
       var results = TmdbFactory.movieSearch('SDF');
       httpBackend.flush();
       var actualResults = results.$object.results;
-      var expectedResults = searchResults.results;
+      var expectedResults = mocks.searchResults.results;
       var config = TmdbFactory.configuration.$object[0];
       expect(config).toBeDefined();
       expect(actualResults).toEqual(expectedResults);
@@ -218,7 +109,7 @@ describe('tmdb Factory', function() {
       var results = TmdbFactory.movieInfo(550);
       httpBackend.flush();
       var actualResults = results.$object;
-      var expectedResults = infoResults;
+      var expectedResults = mocks.infoResults;
       var config = TmdbFactory.configuration.$object[0];
       expect(config).toBeDefined();
       expect(actualResults.title).toEqual(expectedResults.title);
---
+++ client/angular/src/app/index.mock.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var mocks, baseUrl;
+
+beforeEach(function() {
+  baseUrl = 'http://localhost:3000/';
+  mocks = {
+    configUrl: baseUrl + 'configs',
+    configResults: {
+      configs: [{
+        images: {
+          base_url: 'http://image.tmdb.org/t/p/', // jshint ignore:line
+          secure_base_url: 'https://image.tmdb.org/t/p/', // jshint ignore:line
+          backdrop_sizes: ['w300', 'w780', 'w1280', 'original'], // jshint ignore:line
+          logo_sizes: ['w45', 'w92', 'w154', 'w185', 'w300', 'w500', 'original'], // jshint ignore:line
+          poster_sizes: ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original'], // jshint ignore:line
+          profile_sizes: ['w45', 'w185', 'h632', 'original'], // jshint ignore:line
+          still_sizes: ['w92', 'w185', 'w300', 'original'] // jshint ignore:line
+        }
+      }]
+    },
+    allMoviesUrl: baseUrl + 'movies',
+    allMoviesResults: {
+      movies: [{
+        title: 'Fight Club',
+        themoviedbid: 550,
+        id: 1,
+        createdAt: '2015-02-13T05:44:34.000Z',
+        updatedAt: '2015-02-13T05:44:34.000Z'
+      }, {
+        title: 'Guardians of the Galaxy',
+        themoviedbid: 118340,
+        id: 2,
+        createdAt: '2015-02-13T06:19:29.000Z',
+        updatedAt: '2015-02-13T06:19:29.000Z'
+      }]
+    },
+    createMovieUrl: baseUrl + 'movies',
+    createMovieResults: {
+      movie: {
+        title: 'The Matrix',
+        themoviedbid: 603,
+        id: 3,
+        createdAt: '2015-02-13T05:44:34.000Z',
+        updatedAt: '2015-02-13T05:44:34.000Z'
+      }
+    },
+    searchUrl: baseUrl + 'movies/search/SDF',
+    searchResults: {
+      page: 1,
+      results: [{
+        adult: false,
+        backdrop_path: '/qLxJlnfZ9b2BMzvhzvnJMBi7EGH.jpg', // jshint ignore:line
+        id: 14114,
+        original_title: 'You Got Served', // jshint ignore:line
+        release_date: '2004-01-30', // jshint ignore:line
+        poster_path: '/3ESy6TiBPSYIbEES1zNspdYtMtw.jpg', // jshint ignore:line
+        popularity: 0.33499858390984,
+        title: 'You Got Served',
+        video: false,
+        vote_average: 6.3, // jshint ignore:line
+        vote_count: 9 // jshint ignore:line
+      }],
+      total_pages: 1, // jshint ignore:line
+      total_results: 1 // jshint ignore:line
+    },
+    infoUrl: baseUrl + 'movies/info/550',
+    infoResults: {
+      adult: false,
+      backdrop_path: '/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg', // jshint ignore:line
+      belongs_to_collection: null, // jshint ignore:line
+      budget: 63000000,
+      genres: [{
+        id: 18,
+        name: 'Drama'
+      }],
+      homepage: '',
+      id: 550,
+      imdb_id: 'tt0137523', // jshint ignore:line
+      original_language: 'en', // jshint ignore:line
+      original_title: 'Fight Club', // jshint ignore:line
+      overview: 'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground " fight clubs " forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
+      popularity: 3.16634464772838,
+      poster_path: '/2lECpi35Hnbpa4y46JX0aY3AWTy.jpg', // jshint ignore:line
+      production_companies: [{ // jshint ignore:line
+        name: '20th Century Fox',
+        id: 25
+      }, {
+        name: 'Fox 2000 Pictures',
+        id: 711
+      }, {
+        name: 'Regency Enterprises',
+        id: 508
+      }],
+      production_countries: [{ // jshint ignore:line
+        iso_3166_1: 'DE', // jshint ignore:line
+        name: 'Germany'
+      }, {
+        iso_3166_1: 'US', // jshint ignore:line
+        name: 'United States of America'
+      }],
+      release_date: '1999-10-14', // jshint ignore:line
+      revenue: 100853753,
+      runtime: 139,
+      spoken_languages: [{ // jshint ignore:line
+        iso_639_1: 'en', // jshint ignore:line
+        name: 'English'
+      }],
+      status: 'Released',
+      tagline: 'How much can you know about yourself if you\'ve never been in a fight?',
+      title: 'Fight Club',
+      video: false,
+      vote_average: 7.8, // jshint ignore:line
+      vote_count: 3286 // jshint ignore:line
+    }
+  };
+});
