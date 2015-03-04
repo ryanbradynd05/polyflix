ebffee4fd4e9c1cfb456054939ed6bf8c9796fbb
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-03 23:30:45 -0500

    Add ember routes

--- client/ember/app/router.js
+++ client/ember/app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('movies', function() {
+    this.route('search');
+    this.route('view');
+  });
 });
 
 export default Router;
