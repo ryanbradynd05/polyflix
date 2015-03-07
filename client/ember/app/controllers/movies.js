fd677aaba4b34c4100a9f44aba724b9ef66198c4
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-06 23:38:00 -0500

    Add action placeholders

--- client/ember/app/controllers/movies.js
+++ client/ember/app/controllers/movies.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    deleteMovie: function(movie) {
+      console.log('deleteMovie', movie);
+    },
+    viewMovie: function(movie) {
+      console.log('viewMovie', movie);
+    }
+  }
 });
