8a1fce4ba6a41d97de88cf9c57412cd9c49725ff
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-07 23:51:13 -0500

    Show search results on search page

--- client/ember/app/controllers/movies/search.js
+++ client/ember/app/controllers/movies/search.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
+import config from '../../config/environment';
 
 export default Ember.Controller.extend({
+  query: '',
+  actions: {
+    search: function() {
+      var query = this.get('query');
+      console.log('search', query);
+      Ember.$.get(config.restURL + '/movies/search/' + query)
+      .done(results => {
+        console.log('search results',results);
+        this.set('movies',results.results);
+      });
+    }
+  }
 });
