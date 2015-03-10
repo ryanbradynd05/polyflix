1a955f0863e07291108f8e10c4688938988b8de0
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-09 23:21:44 -0400

    Add fix for null images

--- client/ember/app/models/tmdb-movie.js
+++ client/ember/app/models/tmdb-movie.js
@@ -5,26 +5,26 @@ export default Ember.Object.extend({
   posterUrl: function() {
     var posterPath = this.get('poster_path');
     var config = this.get('tmdbConfig');
-    console.log('config',this,config);
-    if (posterPath === undefined) {
+    if (posterPath === undefined || posterPath === null) {
       return 'https://d3a8mw37cqal2z.cloudfront.net/assets/f996aa2014d2ffddfda8463c479898a3/images/no-poster-w185.jpg';
+    } else {
+      var posterSizes = config.get('images.poster_sizes');
+      var baseUrl = config.get('images.base_url');
+      var posterDefaultSize = this.get('posterDefaultSize');
+      return baseUrl + posterSizes[posterDefaultSize] + posterPath;
     }
-    var posterSizes = config.get('images.poster_sizes');
-    var baseUrl = config.get('images.base_url');
-    var posterDefaultSize = this.get('posterDefaultSize');
-    return baseUrl + posterSizes[posterDefaultSize] + posterPath;
   }.property('poster_path'),
   backdropDefaultSize: 2,
   backdropUrl: function() {
     var backdropPath = this.get('backdrop_path');
     var config = this.get('tmdbConfig');
-    console.log('config',config);
-    if (backdropPath === undefined) {
+    if (backdropPath === undefined || backdropPath === null) {
       return '';
+    } else {
+        var backdropSizes = config.get('images.backdrop_sizes');
+        var baseUrl = config.get('images.base_url');
+        var backdropDefaultSize = this.get('backdropDefaultSize');
+        return baseUrl + backdropSizes[backdropDefaultSize] + backdropPath;
     }
-    var backdropSizes = config.get('images.backdrop_sizes');
-    var baseUrl = config.get('images.base_url');
-    var backdropDefaultSize = this.get('backdropDefaultSize');
-    return baseUrl + backdropSizes[backdropDefaultSize] + backdropPath;
   }.property('backdrop_path')
 });
