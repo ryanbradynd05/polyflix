beb2e11be67134edbcf47ff32750df8770b3a075
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-09 22:06:00 -0500

    Load search results as tmdbMovie models

--- client/ember/app/controllers/movies/search.js
+++ client/ember/app/controllers/movies/search.js
@@ -1,16 +1,26 @@
 import Ember from 'ember';
 import config from 'polyflix/config/environment';
+import TmdbMovie from 'polyflix/models/tmdb-movie';
 
 export default Ember.Controller.extend({
   query: '',
   actions: {
     search: function() {
+      var movies = [];
+      this.set('movies', movies);
       var query = this.get('query');
+      var tmdbConfig = this.tmdbConfig;
       console.log('search', query);
       Ember.$.get(config.restURL + '/movies/search/' + query)
-      .done(results => {
-        console.log('search results',results);
-        this.set('movies',results.results);
+      .done(response => {
+        var results = response.results;
+        results.forEach(function(movieData) {
+          movieData.tmdbConfig = tmdbConfig;
+          var tmdbMovie = TmdbMovie.create(movieData);
+          movies.pushObject(tmdbMovie);
+        });
+        console.log('search results', movies);
+        this.set('movies', movies);
       });
     },
     getInfo: function(movie) {
