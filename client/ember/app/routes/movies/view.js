bd92de5dc76170f28da25b0aa0698287b47fe53f
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-10 22:15:00 -0500

    Load movie info from server

--- client/ember/app/routes/movies/view.js
+++ client/ember/app/routes/movies/view.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
+import config from 'polyflix/config/environment';
+import TmdbMovie from 'polyflix/models/tmdb-movie';
 
 export default Ember.Route.extend({
+    model: function(params) {
+        var id = params.id;
+        console.log('info', id);
+        return Ember.$.get(config.restURL + '/movies/info/' + id)
+        .done(response => {
+          var tmdbMovie = TmdbMovie.create(response);
+          console.log('info result:', tmdbMovie);
+          return tmdbMovie;
+        });
+    }
 });
