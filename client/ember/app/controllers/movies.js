336c3bfe688fd53007d5f5e04436e7d41b6687f7
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-11 22:09:00 -0500

    Remove unused action

--- client/ember/app/controllers/movies.js
+++ client/ember/app/controllers/movies.js
@@ -4,9 +4,6 @@ export default Ember.Controller.extend({
   actions: {
     deleteMovie: function(movie) {
       console.log('deleteMovie', movie);
-    },
-    viewMovie: function(movie) {
-      console.log('viewMovie', movie);
     }
   }
 });
