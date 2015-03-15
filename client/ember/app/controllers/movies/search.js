245d6698ac6b66e0a2fb4a1ece66e4e14ec1d8a3
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-14 22:21:00 -0500

    Use Ember Array for movie search results

--- client/ember/app/controllers/movies/search.js
+++ client/ember/app/controllers/movies/search.js
@@ -4,13 +4,14 @@ import TmdbMovie from 'polyflix/models/tmdb-movie';
 
 export default Ember.Controller.extend({
   query: '',
+  movies: Ember.A(),
   displayModal: function(action) {
     Ember.$('#movieInfoModal').modal(action);
   },
   actions: {
     search: function() {
-      var movies = [];
-      this.set('movies', movies);
+      var movies = this.get('movies');
+      movies.clear();
       var query = this.get('query');
       var tmdbConfig = this.tmdbConfig;
       Ember.$.get(config.restURL + '/movies/search/' + query)
@@ -21,7 +22,7 @@ export default Ember.Controller.extend({
           var tmdbMovie = TmdbMovie.create(movieData);
           movies.pushObject(tmdbMovie);
         });
-        this.set('movies', movies);
+        // this.set('movies', movies);
       });
     },
     getInfo: function(movie) {
