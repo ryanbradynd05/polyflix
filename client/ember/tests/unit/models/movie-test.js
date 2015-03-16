711de598c8f0a1bb4685b864d3a76f21acdfc22b
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-15 21:32:00 -0500

    Add test for movie model

--- client/ember/tests/unit/models/movie-test.js
+++ client/ember/tests/unit/models/movie-test.js
@@ -2,14 +2,29 @@ import {
   moduleForModel,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
+import startApp from 'polyflix/tests/helpers/start-app';
+
+var App;
 
 moduleForModel('movie', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: [],
+  setup: function(){
+    App = startApp();
+  },
+  teardown: function(){
+    Ember.run(App, 'destroy');
+  }
 });
 
-test('it exists', function(assert) {
-  var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
+test('It stores movies', function () {
+    var store = this.store();
+    var record = null;
+    Ember.run(function() {
+        store.createRecord('movie', {id: 1, title: 'Fight Club', themoviedbid: 550});
+        record = store.find('movie', 1);
+    });
+    equal(record.get('title'), 'Fight Club');
+    equal(record.get('themoviedbid'), 550);
 });
