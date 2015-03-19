21ade85a1eefb8d84e86d03960e75d33481c6be0
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-18 20:19:00 -0500

    Fix test for tmdbMovie & config changes

--- client/ember/tests/unit/models/tmdb-movie-test.js
+++ client/ember/tests/unit/models/tmdb-movie-test.js
@@ -53,17 +53,17 @@ var movieMock = {
   title: 'Fight Club',
   video: false,
   vote_average: 7.8, // jshint ignore:line
-  vote_count: 3286, // jshint ignore:line,
-  tmdbConfig: {
-    images: {
-      base_url: 'http://image.tmdb.org/t/p/', // jshint ignore:line
-      secure_base_url: 'https://image.tmdb.org/t/p/', // jshint ignore:line
-      backdrop_sizes: ['w300', 'w780', 'w1280', 'original'], // jshint ignore:line
-      logo_sizes: ['w45', 'w92', 'w154', 'w185', 'w300', 'w500', 'original'], // jshint ignore:line
-      poster_sizes: ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original'], // jshint ignore:line
-      profile_sizes: ['w45', 'w185', 'h632', 'original'], // jshint ignore:line
-      still_sizes: ['w92', 'w185', 'w300', 'original'] // jshint ignore:line
-    }
+  vote_count: 3286 // jshint ignore:line
+};
+var configMock = {
+  images: {
+    base_url: 'http://image.tmdb.org/t/p/', // jshint ignore:line
+    secure_base_url: 'https://image.tmdb.org/t/p/', // jshint ignore:line
+    backdrop_sizes: ['w300', 'w780', 'w1280', 'original'], // jshint ignore:line
+    logo_sizes: ['w45', 'w92', 'w154', 'w185', 'w300', 'w500', 'original'], // jshint ignore:line
+    poster_sizes: ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original'], // jshint ignore:line
+    profile_sizes: ['w45', 'w185', 'h632', 'original'], // jshint ignore:line
+    still_sizes: ['w92', 'w185', 'w300', 'original'] // jshint ignore:line
   }
 };
 var movie = null;
@@ -80,6 +80,8 @@ test('It creates tmdb-movies', function() {
 test('Gets poster URL', function() {
   Ember.run(function() {
     movie = TmdbMovie.create(movieMock);
+    var config = Ember.Object.create(configMock);
+    movie.set('config', config);
   });
   equal(movie.get('posterUrl'), 'http://image.tmdb.org/t/p/w185/2lECpi35Hnbpa4y46JX0aY3AWTy.jpg');
 });
@@ -87,6 +89,8 @@ test('Gets poster URL', function() {
 test('Gets backdrop URL', function() {
   Ember.run(function() {
     movie = TmdbMovie.create(movieMock);
+    var config = Ember.Object.create(configMock);
+    movie.set('config', config);
   });
   equal(movie.get('backdropUrl'), 'http://image.tmdb.org/t/p/w1280/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg');
 });
