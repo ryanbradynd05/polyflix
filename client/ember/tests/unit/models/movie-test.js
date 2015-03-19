a4285431c0a02619ed5fb6422bdedc89d828996f
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-18 20:25:00 -0500

    Movie model test refractor

--- client/ember/tests/unit/models/movie-test.js
+++ client/ember/tests/unit/models/movie-test.js
@@ -1,8 +1,15 @@
 import {
   moduleForModel,
   test
-} from 'ember-qunit';
+}
+from 'ember-qunit';
 import Ember from 'ember';
+
+var movieMock = {
+  id: 1,
+  title: 'Fight Club',
+  themoviedbid: 550
+};
 
 moduleForModel('movie');
 
@@ -10,13 +17,9 @@ test('It stores movies', function() {
   var store = this.store();
   var record = null;
   Ember.run(function() {
-    store.createRecord('movie', {
-      id: 1,
-      title: 'Fight Club',
-      themoviedbid: 550
-    });
+    store.createRecord('movie', movieMock);
     record = store.find('movie', 1);
   });
-  equal(record.get('title'), 'Fight Club');
-  equal(record.get('themoviedbid'), 550);
+  equal(record.get('title'), movieMock.title);
+  equal(record.get('themoviedbid'), movieMock.themoviedbid);
 });
