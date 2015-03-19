08e299491da581b1d64d8fda499a0fd94d8427a1
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-18 21:15:00 -0500

    Add test for config model

--- client/ember/tests/unit/models/config-test.js
+++ client/ember/tests/unit/models/config-test.js
@@ -1,12 +1,64 @@
 import {
   moduleForModel,
   test
-} from 'ember-qunit';
+}
+from 'ember-qunit';
+import Ember from 'ember';
+
+var configMock = {
+  id: 1,
+  images: {
+    base_url: "http://image.tmdb.org/t/p/",
+    secure_base_url: "https://image.tmdb.org/t/p/",
+    backdrop_sizes: [
+      "w300",
+      "w780",
+      "w1280",
+      "original"
+    ],
+    logo_sizes: [
+      "w45",
+      "w92",
+      "w154",
+      "w185",
+      "w300",
+      "w500",
+      "original"
+    ],
+    poster_sizes: [
+      "w92",
+      "w154",
+      "w185",
+      "w342",
+      "w500",
+      "w780",
+      "original"
+    ],
+    profile_sizes: [
+      "w45",
+      "w185",
+      "h632",
+      "original"
+    ],
+    still_sizes: [
+      "w92",
+      "w185",
+      "w300",
+      "original"
+    ]
+  }
+};
 
 moduleForModel('config');
 
-test('it exists', function(assert) {
-  var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
+test('It stores movies', function() {
+  var store = this.store();
+  var record = null;
+  Ember.run(function() {
+    store.createRecord('config', configMock);
+    record = store.find('config', 1);
+  });
+  equal(record.get('images.base_url'), configMock.images.base_url);
+  equal(record.get('images.secure_base_url'), configMock.images.secure_base_url);
+  equal(record.get('images.backdrop_sizes'), configMock.images.backdrop_sizes);
 });
